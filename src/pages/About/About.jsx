265fa3346a916cec73d910d17fb560a153d78f49
Saranshverma234 <vermasaransh124@gmail.com>
2025-04-29 16:56:13 +0530

    changes3

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,27 +3,26 @@ import aboutImg from "../../images/about-img.jpg"; // Importing the about image;
 // About component to display information about BookHub
 const About = () => {
   return (
-    <section className="max-w-[1200px]  mx-auto">
-      <div className="p-6
-     md:p-12 ">
+    <section className="max-w-[1550px] bg-amber-100  mx-auto">
+      <div className=" p-6 md:p-12 ">
         {/* Section title for the About page */}
         <div className="section-title mb-8">
           <h2 className="text-2xl font-bold  text-blue-950 uppercase">About</h2>
         </div>
 
         {/* Grid layout for the image and text */}
-        <div className="grid gap-8 md:gap-16 md:grid-cols-2">
+        <div className="grid gap-8  md:gap-12 md:grid-cols-2">
           {/* About image section */}
-          <div className="about-img">
+          <div className="about-img ">
             <img
               src={aboutImg}
               alt="About E-book Library"
-              className="max-w-full h-auto transition-transform transform hover:scale-105 hover:shadow-2xl"
+              className="max-w-md h-auto transition-transform transform hover:scale-105 hover:shadow-2xl"
             />
           </div>
 
           {/* About text section */}
-          <div className="about-text">
+          <div className="about-text text-7xl">
             <h2 className="text-2xl md:text-3xl font-semibold text-blue-950 mb-4">
               About E-book Library
             </h2>
